a56a1c52c545f76e4907bc9b84ce2bc62f5b0bdd
Sunggun Yu <sunggun.dev@gmail.com>
2016-11-02 17:41:57 -0400

    Init
    Signed-off-by: Sunggun Yu <sunggun.dev@gmail.com>

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM ubuntu:14.04
+MAINTAINER Sunggun Yu <sunggun.dev@gmail.com>
+
+ENV CHEF_SERVER_VERSION 12.10.0
+ENV CHEF_SERVER_DOWNLOAD_URL https://packages.chef.io/stable/ubuntu/14.04/chef-server-core_"$CHEF_SERVER_VERSION"-1_amd64.deb
+ENV CHEF_SERVER_DOWNLOAD_SHA1 95fce9f167972418b3f06b9e5fe95f8a3f0e5361
+
+RUN set -x \
+    && mkdir -p /var/opt/chef-server
+
+WORKDIR /var/opt/chef-server
+
+# Install and wget and download Chef-Server package file
+RUN set -x \
+    && apt-get update \
+    && apt-get install -y wget \
+    && rm -rf /var/lib/apt/lists/* \
+    && wget -O chef-server-core_"$CHEF_SERVER_VERSION"-1_amd64.deb "$CHEF_SERVER_DOWNLOAD_URL" \
+    && echo "$CHEF_SERVER_DOWNLOAD_SHA1 chef-server-core_$CHEF_SERVER_VERSION-1_amd64.deb" | sha1sum -c - \
+    && dpkg -i chef-server-core_"$CHEF_SERVER_VERSION"-1_amd64.deb \
+    && rm chef-server-core_"$CHEF_SERVER_VERSION"-1_amd64.deb
+
+COPY entrypoint.sh /
+
+# Execute entrypoint
+ENTRYPOINT ["/entrypoint.sh"]
+
+# Expose ports
+EXPOSE 443
---
+++ entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -x
+
+sysctl -w kernel.shmmax=17179869184
+
+ulimit -s unlimited
+
+# Start this so that chef-server-ctl sv-related commands can interact with its services via runsv
+/opt/opscode/embedded/bin/runsvdir-start &
+
+# Run all of the services
+chef-server-ctl reconfigure
+
+## Do initial installation and configuration
+if [ ! -f /var/opt/chef-server/.configured ]; then
+
+#    # Install Chef Manage
+#    chef-server-ctl install chef-manage
+#
+#    # Install Chef Reporting
+#    chef-server-ctl install opscode-reporting
+#
+#    # Reconfigure the Chef Server
+#    chef-server-ctl reconfigure
+#
+#    # Reconfigure Chef Manage
+#    chef-manage-ctl reconfigure --accept-license
+#
+#    # Reconfigure Chef Reporting
+#    opscode-reporting-ctl reconfigure
+
+    # Create a default admin user
+    chef-server-ctl user-create admin FIRST_NAME LAST_NAME admin@example.com 'admin123' --filename /var/opt/chef-server/admin.pem
+
+    # Touch flag file that is indicating initial chef server configuration has been done
+    echo "configured" > /var/opt/chef-server/.configured
+else
+    # Run all of the services
+    chef-server-ctl reconfigure
+fi
+
+tail -f /var/log/opscode/*/current
+
+exec "$@"
